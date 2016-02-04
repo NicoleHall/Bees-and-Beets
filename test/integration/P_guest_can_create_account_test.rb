e00168aa7d1c64df26f69380a9cb7d643696d48c
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-04 11:49:58 -0700

    Begin user creation

---
+++ test/integration/P_guest_can_create_account_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class GuestCanCreateAccountTest < ActionDispatch::IntegrationTest
+  test "guest can create account and sees profile" do
+    # add_items_to_cart_and_visit_shopping_cart(1)
+    # @item = @items.first
+    # assert page.has_content?(@item.title)
+
+    visit "/"
+    # save_and_open_page
+    first(:link, "Log In/Sign Up").click
+    click_on "Create Account"
+
+    assert_equal new_user_path, current_path
+
+    fill_in "First name", with: "Brenna"
+    fill_in "Last name", with: "Martenson"
+    fill_in "Email address", with: "brenna@awesome.com"
+    fill_in "Street address", with: "123 Maple Drive"
+    fill_in "City", with: "Denver"
+    select('Colorado', from: 'user_state')
+    fill_in "Zipcode", with: "80231"
+    fill_in "Username", with: "brenna"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Create Account"
+    user = User.last
+    assert_equal "/dashboard", current_path
+
+    within(".nav-wrapper") do
+      assert page.has_content?("Logged in as #{user.first_name}")
+      refute page.has_content?("Login/Signup")
+      assert page.has_content?("Logout")
+    end
+
+    assert page.has_content?("#{user.first_name}")
+    assert page.has_content?("#{user.last_name}")
+    assert page.has_content?("#{user.email_address}")
+    assert page.has_content?("#{user.addresses.first.street}")
+    assert page.has_content?("#{user.addresses.first.city}")
+    assert page.has_content?("#{user.addresses.first.state}")
+    assert page.has_content?("#{user.addresses.first.zipcode}")
+
+    assert page.has_button?("Edit Info")
+    assert page.has_button?("Order History")
+  end
+end
