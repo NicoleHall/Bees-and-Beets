581322c6a602b965e898ac8758dc3e53a947a2f9
martensonbj <martensonbj@gmail.com>
2016-02-08 13:36:15 -0700

    Vendor can add item complete

--- test/integration/P_vendor_can_add_an_item_test.rb
+++ test/integration/P_vendor_can_add_an_item_test.rb
@@ -27,11 +27,9 @@ class VendorCanAddAnItemTest < ActionDispatch::IntegrationTest
     click_on "Create Item"
 
     item = Item.last
-    binding.pry
-    assert_equal vendor, item.user
+    assert_equal vendor, item.vendor
 
-    assert_equal vendor_item_path(vendor: vendor.url, id: item.id), current_path
-
+    assert_equal vendor_items_path(vendor: vendor.url), current_path
     assert page.has_link?("Edit Item")
     assert page.has_link?("Delete Item")
     assert page.has_content? item.title
@@ -41,28 +39,29 @@ class VendorCanAddAnItemTest < ActionDispatch::IntegrationTest
   end
 
   test "guest cannot add an item" do
-    skip
+    vendor = create(:vendor_with_items, status: 1)
     user = create(:user)
-    store = create(:vendor_with_items, status: 1)
-    ApplicationController.any_instance.stubs(:current_user).returns(user)
 
-    visit vendor_items_path(vendor: store.url)
-    assert page.has_content?(store.item.first.title)
-    refute page.has_link?("Create Item")
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    user = create(:user)
+
+    visit vendor_items_path(vendor: vendor.url)
+    assert page.has_content?(vendor.items.first.title)
+    refute page.has_link?("Add Item")
+    refute page.has_button?("Edit Item")
+    refute page.has_button?("Delete Item")
   end
 
   test "item can have a price with commas and dollar signs" do
-    skip
-    vendor = create(:vendor)
+    vendor = create(:vendor_with_user, status: 1)
+    user = vendor.users.first
+    category = create(:category)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
     image_path = "https://petenelson.com/wp-content" \
                  "/uploads/2011/08/ron-swanson-meat.jpg"
 
-    category = create(:category)
-    ApplicationController.any_instance.stubs(:current_user).returns(vendor)
-
-    visit vendor_dashboard_path
-    click_on "Create Item"
-    assert_equal vendor_items_path(vendor: vendor.url), current_path
+    visit new_vendor_item_path(vendor: vendor.url)
 
     fill_in "Title", with: "Meat"
     fill_in "Image path", with: image_path
@@ -71,26 +70,23 @@ class VendorCanAddAnItemTest < ActionDispatch::IntegrationTest
     select category.name, from: "item_category_id"
     click_on "Create Item"
 
-    assert_equal vendor_item_path(vendor: vendor.url, id: Item.last.id), current_path
+    assert_equal vendor_items_path(vendor: vendor.url), current_path
     assert_equal 1, Item.count
     assert page.has_content? Item.last.price
-    assert_equal vendor, Item.last.user
+    assert_equal vendor, Item.last.vendor
     assert_equal 100000, Item.last.price
   end
 
   test "vendor cannot add item with missing fields" do
-    skip
-    vendor = create(:vendor)
+    vendor = create(:vendor_with_user, status: 1)
+    user = vendor.users.first
     category = create(:category)
-    ApplicationController.any_instance.stubs(:current_user).returns(vendor)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
 
     image_path = "https://petenelson.com/wp-content" \
                  "/uploads/2011/08/ron-swanson-meat.jpg"
 
-    visit vendor_dashboard_path
-    click_on "Create Item"
-
-    assert_equal vendor_items_path(vendor: vendor.url), current_path
+    visit new_vendor_item_path(vendor: vendor.url)
 
     fill_in "Image path", with: image_path
     fill_in "Price", with: "10"
