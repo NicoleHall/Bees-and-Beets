88e70f16903d42a87df8824fa2e16f2afec731bb
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-11 09:40:28 -0700

    Push category logic down the stack

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.order(updated_at: :desc).includes(:items)
+    @categories = Category.desc_order_include_items
   end
 end
