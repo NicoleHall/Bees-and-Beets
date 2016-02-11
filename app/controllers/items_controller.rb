fae2b3b68872a613ed5520e8ef7a8fd03cc2db6c
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-10 23:21:42 -0700

    Fix item scoping on categories index page, remove add to cart button if item vendor is closed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-      @category = Category.find_by(url: params[:category_url])
-      @items = @category.items
+    @category = Category.find_by(url: params[:category_url])
+    @items = @category.items.joins(:vendor).where("vendors.status = ?", 1)
   end
 end
